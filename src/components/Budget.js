9dc1b5b042ab60fb7d3a99b445d0e8576bc0afcf
Rohit Choudhari <rohit.choudhari1@gmail.com>
2023-05-26 18:09:31 -0400

    Task 1 & Task 2

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,44 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const {budget} = useContext(AppContext);
+    const {budget,dispatch,expenses} = useContext(AppContext);
+    const [oldBudget, setOldBudget] = useState(0);
+    const totalExpenses = expenses.reduce((total, item) =>{
+        return (total = total + item.cost);
+    },0)
+
+    const setBudget = (value) => {
+        setOldBudget(budget);
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: value,
+        });
+        if(oldBudget>20000){
+            alert("The value cannot exceed more than Rs. 20,000");
+            return;
+        }
+        if(oldBudget<totalExpenses){
+            alert("You cannot reduce budget value lower than the spending");
+            return;
+        }
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: oldBudget,
+        });
+    };
+
     return(
         <div className='alert alert-secondary'>
-            <span>Budget: Rs. {budget}/-</span>
+            <span>Budget: Rs. 
+                <input 
+                    type="number" 
+                    id="budget"
+                    step="10"
+                    value={budget}
+                    onChange={(event) => setBudget(event.target.value)}>
+                </input>/-
+            </span>
         </div>
     );
 }
